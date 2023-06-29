39526dc425114a67aa064fa4e84c96e711598049
pitou77 <71866126+pitou77@users.noreply.github.com>
2023-06-29 13:09:06 +0700

    Update and rename salink_sbFIX.js to salink_sb.js

--- salink_sb.js
+++ salink_sb.js
@@ -18,7 +18,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi.replace('streamsb','sbembed'));
+{var encryptedUrl=Base64.encode(a_to_vi.replace('streamsb.com','streamsb.net'));
 
 var hardencryptedUrl=Base64.encode(encryptedUrl);
 
